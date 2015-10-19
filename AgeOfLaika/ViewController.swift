75fb981c5b70f25d82158f1c3d10bbfe25017361
Ted Ngac <duy.a.ngac@icloud.com>
2015-10-19 21:41:46 +0800

    first commit

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageInHumanYearsTxtFld: UITextField!
+    @IBOutlet weak var humanYearsLbl: UILabel!
+    @IBOutlet weak var warningLbl: UILabel!
+    @IBOutlet weak var dogYearsLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertBtnPressed(sender: UIButton) {
+        let dogYears : Double = 4
+        let humanYears = Double(ageInHumanYearsTxtFld.text!)
+        var inDogYears : Double
+        
+        if (humanYears == nil) {
+            warningLbl.hidden = false
+            warningLbl.text = "Please enter the age ..."
+        } else {
+            warningLbl.hidden = true
+            
+            if (humanYears == 1) {
+                inDogYears = 10.5
+            } else if (humanYears == 2) {
+                inDogYears = 21
+            } else {
+                inDogYears = 21 + (humanYears! - 2) * dogYears
+            }
+            
+            ageInHumanYearsTxtFld.resignFirstResponder()
+            ageInHumanYearsTxtFld.text = ""
+            
+            humanYearsLbl.hidden = false
+            dogYearsLbl.hidden = false
+            
+            humanYearsLbl.text = "\(humanYears!)"
+            dogYearsLbl.text = "\(inDogYears)"
+        }
+        
+        
+    }
 
 }
 
